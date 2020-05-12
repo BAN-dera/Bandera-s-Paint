83f50b0e5189d94b8431fa9639d76b693019191c
BAN-Coder <65175268+BAN-dera@users.noreply.github.com>
2020-05-12 20:29:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,60 +28,39 @@ from kivy.graphics import (Color, Ellipse, Rectangle, Line)
 
 class PainterWidget(Widget):
 	def on_touch_down(self, touch):
-		tit = PaintApp.textinput.text
-		if tit.lower() == "grey" or tit.lower() == "серый" or tit.lower() == "color":
-			color = (0.33, 0.33, 0.33, 1)
-		elif tit.lower() == "red" or tit.lower() == "красный":
-			color = (1, 0, 0, 1)
-		elif tit.lower() == "green" or tit.lower() == "зелёный" or tit.lower() == "зеленый":
-			color = (0, 1, 0, 1)
-		elif tit.lower() == "blue" or tit.lower() == "синий":
-			color = (0, 0, 1, 1)
-		elif tit.lower() == "yellow" or tit.lower() == "жёлтый" or tit.lower() == "желтый":
-			color = (1, .84, 0, 1)
-		elif tit.lower() == "cyan" or tit.lower() == "бирюзовый":
-			color = (0, 1, .88, 1)
-		elif tit.lower() == "lightblue" or tit.lower() == "light blue" or tit.lower() == "светлоголубой"or tit.lower() == "светло-голубой" or tit.lower() == "светло голубой":
-			color = (0, .35, 1, 1)
-		elif tit.lower() == "pink" or tit.lower() == "розовый":
-			color = (1, 0, .58, 1)
-		elif tit.lower() == "violet" or tit.lower() == "фиолетовый":
-			color = (.57, 0, 1, 1)
-		elif tit.lower() == "lightviolet" or tit.lower() == "light-violet" or tit.lower() == "light violet" or tit.lower() == "светло фиолетовый" or tit.lower() == "светло-фиолетовый" or tit.lower() == "фиалковый":
-			color = (.77, 0, 1, 1)
-		elif tit.lower() == "orange" or tit.lower() == "оранжевый":
-			color = (1, .42, 0, 1)
-		elif tit.lower() == "white" or tit.lower() == "белый":
-			color = (1, 1, 1, 1)
-		elif tit.lower() == "black" or tit.lower() == "черный" or tit.lower() == "чёрный":
-			color = (0, 0, 0, 1)
-		else:
-			color = (1, 1, 1, 1)
+		slider_r_control = BanderasPaintApp.slider_color_r.value
+		slider_g_control = BanderasPaintApp.slider_color_g.value
+		slider_b_control = BanderasPaintApp.slider_color_b.value
+		slider_a_control = BanderasPaintApp.slider_color_a.value
+		color = (slider_r_control, slider_g_control, slider_b_control, slider_a_control)
 
-		slider_control = PaintApp.slider.value
+		slider_control = BanderasPaintApp.slider.value
 
 		with self.canvas:
 			Color(*color)
 			rad = slider_control
 			window_width, window_height = Window.size
-			if touch.y < window_height * 9 / 10:
+			if touch.y < window_height * 8.5 / 10:
 				Ellipse(pos = (touch.x - rad/2, touch.y - rad/2), size = (rad, rad))
 				touch.ud["line"] = Line(points = (touch.x, touch.y), width = rad / 2)
 
 	def on_touch_move(self, touch):
 		window_width, window_height = Window.size
-		if touch.y < window_height * 9 / 10:
+		if touch.y < window_height * 8.5 / 10:
 			touch.ud["line"].points += (touch.x, touch.y)
 
-class PaintApp(App):
-	textinput = TextInput(text = "Color", multiline = False)
+class BanderasPaintApp(App):
+	slider_color_r = Slider(value = 0, min = 0, max = 1)
+	slider_color_g = Slider(value = 0, min = 0, max = 1)
+	slider_color_b = Slider(value = 0, min = 0, max = 1)
+	slider_color_a = Slider(value = 0, min = 0, max = 1)
 	slider = Slider(value = 8, min = 0, max = 36)
 	def build(self):
 		# Window layout
 		parent = BoxLayout(orientation = "vertical")
 
 		# Layout for buttons
-		gl = GridLayout(cols = 6, size_hint = (1, 0.1))
+		gl = GridLayout(cols = 6, size_hint = (1, 0.15))
 
 		# Button - Clear and Save
 		self.btn_clear = Button(text = "Clear", on_press = self.clear_canvas)
@@ -89,11 +68,17 @@ class PaintApp(App):
 		gl.add_widget(self.btn_clear)
 		gl.add_widget(self.btn_save)
 
-		# Input-Color
-		gl.add_widget(PaintApp.textinput)
+		# Slider Colors
+		bl_colors = BoxLayout(orientation = "vertical")
+		bl_colors.add_widget(BanderasPaintApp.slider_color_r)
+		bl_colors.add_widget(BanderasPaintApp.slider_color_g)
+		bl_colors.add_widget(BanderasPaintApp.slider_color_b)
+		bl_colors.add_widget(BanderasPaintApp.slider_color_a)
+
+		gl.add_widget(bl_colors)
 
 		# Slider
-		gl.add_widget(PaintApp.slider)
+		gl.add_widget(BanderasPaintApp.slider)
 
 		# Layouts for PainterWidget
 		bl = BoxLayout()
@@ -114,4 +99,4 @@ class PaintApp(App):
 		self.painter.export_to_png("image.png")
 
 if __name__ == "__main__":
-	PaintApp().run()
+	BanderasPaintApp().run()
